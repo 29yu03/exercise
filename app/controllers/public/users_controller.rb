31129c5935f05d4c344687164f41c563f7f1c8ba
YuIshikawa <166091720+29yu03@users.noreply.github.com>
2024-07-05 16:50:51 +0000

    ユーザ情報編集ページ更新

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,6 @@
 class Public::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :configure_password_length, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -7,6 +9,8 @@ class Public::UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    @posts = @user.posts
+
   end
 
   def update
@@ -20,8 +24,16 @@ class Public::UsersController < ApplicationController
 
   private
 
+   def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
-    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :telephone_number, :is_active, :profile_image,)
+    params.require(:user).permit(:introduction, :last_name, :first_name, :last_name_kana, :first_name_kana, :email, :telephone_number, :is_active, :profile_image, :password, :password_confirmation)
+  end
+
+  def configure_password_length
+    @minimum_password_length = Devise.password_length.min
   end
 
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,3 +24,33 @@ $(document).ready(function() {
   });
 });
 
+
+document.addEventListener("turbolinks:load", function() {
+   // ページの読み込みが完了したら実行される関数
+
+  const profileImageInput = document.getElementById('profile_image_input');
+  const currentProfileImage = document.getElementById('current_profile_image');
+  const imagePreview = document.getElementById('image_preview');
+  // ファイル選択ボタンと画像プレビューの要素を取得
+
+  if (profileImageInput) {
+    profileImageInput.addEventListener('change', function(event) {
+    // ファイル選択ボタンにchangeイベントを設定
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      // 選択されたファイルを取得
+
+      // ファイルの読み込みが完了したら実行される処理
+      reader.onload = function(event) {
+        imagePreview.innerHTML = `<img src="${event.target.result}" width="170" height="170" style="border-radius: 10px;">`;
+        // 読み込んだ画像を画像プレビューの要素に表示する
+        currentProfileImage.style.display = 'none';
+        // 現在のプロフィール画像を非表示にする
+
+      };
+
+      reader.readAsDataURL(file);
+      // ファイルをDataURL形式に変換して読み込む
+    });
+  }
+});
