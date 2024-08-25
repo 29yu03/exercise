bc4217fabf19129b7bda0d0e546ca4a03a916b6b
YuIshikawa <166091720+29yu03@users.noreply.github.com>
2024-08-25 01:28:49 +0000

    フォロ-

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -3,12 +3,12 @@ class Public::RelationshipsController < ApplicationController
 
   def create
     @relationship = current_user.active_relationships.create(following_id: params[:following_id])
-    redirect_to user_path(params[:following_id])
+    redirect_to request.referer
   end
 
   def destroy
     @relationship = current_user.active_relationships.find(params[:id])
     @relationship.destroy
-    redirect_to user_path(params[:user_id])
+    redirect_to request.referer
   end
 end
